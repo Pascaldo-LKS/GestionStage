2849c333c87911931ad56875ffdfc13422026c64
Pascal LKS <pasallokossou31@gmail.com>
2026-08-07 10:33:21 +0100

    Céation de l'architecture de mvc

---
+++ core/Database.php
@@ -0,0 +1,13 @@
+<?php 
+class Database {
+    private $hoet ="localhost";
+    private $dbname ="gestion_stage";
+    private $username ="root";
+    private $password ="";
+    private $conn ;
+
+    public function __construct()
+    {
+        
+    }
+}
